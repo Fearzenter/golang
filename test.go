a30239782f2258361cb78954cf4c852846277016
Fearzenter <gergievwot@mail.ru>
2022-03-21 14:38:00 +0300

    test

---
+++ test.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//type store struct {
+//id         int
+//name       string
+//image_link string
+//created_at string
+//updated_at string
+//deleted_at string
+//}
+
+func main() {
+	fmt.Println("Hello Golang!")
+}
