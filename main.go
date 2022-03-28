9b882dba812e2ba8ff14c9f4cc9b864cfedc1425
Fearzenter <gergievwot@mail.ru>
2022-03-28 21:36:52 +0300

    append endpoints

--- main.go
+++ main.go
@@ -6,110 +6,84 @@ import (
 	"os"
 )
 
-/*func test() {
-	{
-		testStore := store{super: super{Id: -1, Name: "Vyasoliy_molochnik", CreatedAt: "22.03.2022", UpdatedAt: "22.03.2022", DeletedAt: "22.03.2022"}, ImageLink: "yandex.ru"}
-		fmt.Println(testStore)
-	} //test store
-	{
-		testStoreType := storeType{super: super{Id: -1, Name: "productoviy", CreatedAt: "22.03.2022", UpdatedAt: "22.03.2022", DeletedAt: "22.03.2022"}, ImageLink: "yandex.ru"}
-		fmt.Println(testStoreType)
-	} //test storeType
-	{
-		links := [...]string{
-			"yandex.ru",
-			"google.com",
-			"vk.com",
-			"youtube.com",
-		}
-		testProduct := product{super: super{Id: -1, Name: "milk", CreatedAt: "22.03.2022", UpdatedAt: "22.03.2022", DeletedAt: "22.03.2022"}, ImageLinks: links}
-		fmt.Println(testProduct)
-	} //test product
-	{
-		testProductType := productType{super: super{Id: -1, Name: "molochka", CreatedAt: "22.03.2022", UpdatedAt: "22.03.2022", DeletedAt: "22.03.2022"}, ImageLink: "yandex.ru"}
-		fmt.Println(testProductType)
-	} //test productType
-	{
-		testProductTypeProduct := productTypeProduct{ProductTypeId: -1, ProductId: -1}
-		fmt.Println(testProductTypeProduct)
-	} //test productTypeProduct
-	{
-		testStoreTypeStore := storeTypeStore{StoreTypeId: -1, StoreId: -1}
-		fmt.Println(testStoreTypeStore)
-	} //test storeTypeStore
-}*/
-
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-func saveFile(x []byte) {
-	err := os.WriteFile("file.json", x, 0644)
-	check(err)
+func saveFile(name string, data []byte) error {
+	return os.WriteFile(name, data, 0644)
 }
 
-func loadFile() {
-	loaded, err := os.ReadFile("file.json")
+func loadFile(name string) {
+	loaded, err := os.ReadFile(name)
 	check(err)
 	fmt.Println(string(loaded))
 }
 
 func main() {
 	{
-		fmt.Println("Hello!\n")
+		{
+			fmt.Println("Hello!\n")
 
-		menu := "Menu:\n" +
-			"1) Save to file\n" +
-			"2) Load from file\n" +
-			"0) Exit"
+			menu := "Menu:\n" +
+				"1) Save to file\n" +
+				"2) Load from file\n" +
+				"0) Exit"
 
-	cycle:
-		for {
-			fmt.Println(menu)
-			var answerMenu int
-			fmt.Fscan(os.Stdin, &answerMenu)
+		cycle:
+			for {
+				fmt.Println(menu)
+				var answerMenu int
+				fmt.Fscan(os.Stdin, &answerMenu)
 
-			switch answerMenu {
-			case 1:
-				{
-					fmt.Println("File saved")
+				switch answerMenu {
+				case 1:
+					{
+						fmt.Println("File saved")
+					}
+				case 2:
+					{
+						fmt.Println("File loaded")
+					}
+				case 0:
+					{
+						fmt.Println("Goodbye")
+						break cycle
+					}
+				default:
+					fmt.Println("Mistake, try again")
 				}
-			case 2:
-				{
-					fmt.Println("File loaded")
-				}
-			case 0:
-				{
-					fmt.Println("Goodbye")
-					break cycle
-				}
-			default:
-				fmt.Println("Mistake, try again")
 			}
+			//test()
+		} // menu
+
+		storeJsonBefore := &Store{super: super{Id: -2, //encoding
+			Name:      "testJson",
+			CreatedAt: "2022",
+			UpdatedAt: "",
+			DeletedAt: ""},
+			ImageLink: "",
 		}
-		//test()
-	} // menu
+		storeJsonAfter, _ := json.Marshal(storeJsonBefore)
+		fmt.Println(storeJsonAfter)
 
-	storeJsonBefore := &store{super: super{Id: -2, //encoding
-		Name:      "testJson",
-		CreatedAt: "2022",
-		UpdatedAt: "",
-		DeletedAt: ""},
-		ImageLink: "",
+		fmt.Println("----------------------------------------------------------------")
+
+		var dat map[string]interface{} //decoding
+		if err := json.Unmarshal(storeJsonAfter, &dat); err != nil {
+			panic(err)
+		}
+		fmt.Println(dat)
+
+		saveFile("file.json", storeJsonAfter) //test
+		loadFile("file.json")
 	}
-	storeJsonAfter, _ := json.Marshal(storeJsonBefore)
-	fmt.Println(storeJsonAfter)
 
-	fmt.Println("----------------------------------------------------------------")
-
-	var dat map[string]interface{} //decoding
-	if err := json.Unmarshal(storeJsonAfter, &dat); err != nil {
-		panic(err)
-	}
-	fmt.Println(dat)
-
-	saveFile(storeJsonAfter) //test
-	loadFile()
+	/*e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))*/
 }
--- types.go
+++ types.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"container/list"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
 type super struct {
 	Id        int
 	Name      string
@@ -8,28 +16,109 @@ type super struct {
 	DeletedAt string
 }
 
-type store struct {
+type ProductRepository interface {
+	CreateProduct(p Product) (Product, error)
+	GetProductByID(id int) (Product, error)
+	UpdateProductById(id int, p Product) (Product, error)
+	DeleteProductByID(id int) (Product, error)
+}
+
+type JsonStorage struct {
+	name     string
+	fileName string
+}
+
+func (j JsonStorage) marshalFile(target interface{}) error {
+	encode, err := json.Marshal(target)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(j.fileName, encode, 0644)
+}
+
+func (j JsonStorage) UnmarshalFile(target interface{}) error {
+	decode, err := ioutil.ReadFile(j.fileName)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(decode, target)
+}
+
+func (j JsonStorage) CreateProduct(p Product) (Product, error) {
+	var products []Product
+	if err := j.UnmarshalFile(&products); err != nil {
+		return Product{}, err
+	}
+	products = append(products, p)
+	if err := j.marshalFile(products); err != nil {
+		return Product{}, errors.New("marshal fail")
+	}
+	fmt.Println(p.Name, "is created")
+	return p, nil
+}
+
+func (j JsonStorage) GetProductByID(id int) (Product, error) {
+	var products []Product
+	if err := j.UnmarshalFile(&products); err != nil {
+		return Product{}, errors.New("unmarshal fail")
+	}
+	fmt.Println(products[id].Name, "is:")
+	return products[id], nil
+}
+
+func (j JsonStorage) UpdateProductById(id int, p Product) ([]Product, error) {
+	var products []Product
+	if err := j.UnmarshalFile(&products); err != nil {
+		return []Product{}, err
+	}
+	if err, _ := j.DeleteProductByID(id); err != nil {
+		return []Product{}, errors.New("delete fail")
+	}
+	products = append(products, p)
+	return products, nil
+}
+
+func (j JsonStorage) DeleteProductByID(id int) ([]Product, error) { //нужно вернуть массив новых продуктов или уд. продукт?
+	var products []Product
+	if err := j.UnmarshalFile(&products); err != nil {
+		return []Product{}, err
+	}
+	if id > 0 && id < len(products) {
+		products = append(products[:id-1], products[id+1:]...)
+	} else if id == 0 {
+		//products = append(products[:0], products[1:]...)    //как удалить нулевой и последний
+	} else {
+		//products = append(products[:1], products[2:]...)
+	}
+	if err := j.marshalFile(products); err != nil {
+		return []Product{}, errors.New("delete fail")
+	}
+	fmt.Println("Product deleted")
+	return products, nil
+}
+
+type Store struct {
 	super
 	ImageLink string
 }
-type storeType struct {
+type StoreType struct {
 	super
 	ImageLink string
 }
-type product struct {
+type Product struct {
 	super
-	ImageLinks [4]string
+	ImageLinks list.List
 }
-type productType struct {
+type ProductType struct {
 	super
 	ImageLink string
 	StoreId   int
 }
-type productTypeProduct struct {
+type ProductTypeProduct struct {
 	ProductTypeId int
 	ProductId     int
 }
-type storeTypeStore struct {
+type StoreTypeStore struct {
 	StoreTypeId int
 	StoreId     int
 }
